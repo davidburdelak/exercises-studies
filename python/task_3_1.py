9580b0e0c349c57b2b1c60cf0e520fd3a677baa5
David Burdelak <44511009+davidburdelak@users.noreply.github.com>
2019-12-08 19:58:20 +0100

    Code cleaning

--- python/task_3_1.py
+++ python/task_3_1.py
@@ -1,22 +1,40 @@
-fileWrite = open('dane.csv','w')
-fileRead = open('dane.txt','r')
+"""
+Replace the data.txt file with the following values:
+
+Isabella
+Roberts
+24
+Taylor
+Brown
+22
+James
+Smith
+23
+
+to the CSV file in this form:
+
+Isabella;Roberts;24
+Taylor;Brown;22
+James;Smith;23
+"""
+
+file_write = open('data.csv','w')
+file_read = open('data.txt','r')
 i=1
 
-all_lines = fileRead.readlines()
+all_lines = file_read.readlines()
 
-#Reading all lines from a text file
-for line in all_lines:
+for line in all_lines: #Reading all lines from a text file
     if (i==1):
-        #The rstrip statement removes the '\ n' character denoting the end of the line.
-        line=line.rstrip()
-        fileWrite.write(line+';')
+        line=line.rstrip() #The rstrip statement removes the '\ n' character denoting the end of the line.
+        file_write.write(line+';')
         i+=1
     elif (i==2):
         line=line.rstrip()
-        fileWrite.write(line+';')
+        file_write.write(line+';')
         i+=1
     elif (i==3):
-        fileWrite.write(line)
+        file_write.write(line)
         i=1
-fileWrite.close()
-fileRead.close()
+file_write.close()
+file_read.close()
