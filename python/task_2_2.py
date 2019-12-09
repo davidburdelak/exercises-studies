876bb002f9413a508b29f1b7a50b7ad57f773600
David Burdelak <44511009+davidburdelak@users.noreply.github.com>
2019-12-09 01:10:20 +0100

    Code cleaning

--- python/task_2_2.py
+++ python/task_2_2.py
@@ -1,11 +1,15 @@
+"""
+Task: Create the script indicating the largest number of 3 entered by the user. Perform the task in a loop so that it is possible to perform the fun until the user is asked Do finish the action and choose y / Y (yes).
+"""
+
 out = False
 while out == False:
     
     print ("Enter three numbers")
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    print(max(a,b,c))
+    number_a = int(input())
+    number_b = int(input())
+    number_c = int(input())
+    print(max(number_a,number_b,number_c))
     choice = input ("Finish the action? (Yes/No)")
     if choice == 'Yes':
         out = True
@@ -14,4 +18,3 @@ while out == False:
     elif choice == "No":
         out = False
         print('Back to the program')
-
