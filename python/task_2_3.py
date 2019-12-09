ed0dcecc0c8e35fe56a4796f04397a40f6756135
David Burdelak <44511009+davidburdelak@users.noreply.github.com>
2019-12-09 01:05:29 +0100

    Code cleaning

--- python/task_2_3.py
+++ python/task_2_3.py
@@ -1,32 +1,50 @@
+"""
+Task: Write a program for calculating the real roots of a quadratic equation. The program should first read the parameters a, b, c of the quadratic equation.
+
+DELTA=b2-4ac
+
+if:
+
+• DELTA<0 no solutions
+
+• DELTA=0 one solution
+
+• DELTA>0 two solutions
+
+An example of the program operation:
+
+Enter a: 1
+Enter b: -4
+Enter c: 4
+one solution: x = 2.0
+
+Please list the number of real solutions and solution values. You can use the math package to calculate the root.
+"""
+
 import math
 
-numberA = int(input('Enter a: '))
-numberB = int(input('Enter b: '))
-numberC = int(input('Enter c: '))
-if (numberA!=0):
-    delta=(numberB*numberB)-(4*numberA*numberC)
+number_A = int(input('Enter a: '))
+number_B = int(input('Enter b: '))
+number_C = int(input('Enter c: '))
+if (number_A!=0):
+    delta=(number_B*number_B)-(4*number_A*number_C)
     if (delta<0):
         result = 'No solution!'
     else: 
         if (delta==0):
-            x1=(-numberB)/(2*numberA)
+            x1=(-number_B)/(2*number_A)
             result = 'Many solutions‚ '+str(x1)
         else:
-            x1 = (-numberB-math.sqrt(delta))/(2*numberA)
-            x2 = (-numberB+math.sqrt(delta))/(2*numberA)
+            x1 = (-number_B-math.sqrt(delta))/(2*number_A)
+            x2 = (-number_B+math.sqrt(delta))/(2*number_A)
             result = ('The equation has 2 roots '+str(x1)+' and '+str(x2))
 else:
-    if(numberB!=0):
-        x1=-numberC/numberB
+    if(number_B!=0):
+        x1=-number_C/numbe_rB
         result='The equation has only 1 root '+str(x1)
     else:
-        if(numberC==0):
+        if(number_C==0):
              result = 'To many roots'
         else:
              result = 'No solution'
 print(result)
- 
-        
-# No information about loop to make in exercise 
- 
-        
