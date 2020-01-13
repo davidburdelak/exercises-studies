077335bb13edccfa12df5abd0706b2e0ae30988c
David Burdelak <davidburdelak@gmail.com>
2020-01-13 14:07:30 +0100

    Code cleaning

--- python/task_additional_2.py
+++ python/task_additional_2.py
@@ -1,4 +1,4 @@
-level=int(input("Podaj poziom choinki do wygenerowania: "))
+level=int(input("Enter the level of the Christmas tree to generate: "))
 height=3
 for i in range(0, level):
     for x in range(1, height):
