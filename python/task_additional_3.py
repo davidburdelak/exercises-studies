48a8a147788559ca25a50e3da7e80dc76dcc75a2
David Burdelak <davidburdelak@gmail.com>
2020-01-17 19:37:18 +0100

    Code cleaning

--- python/task_additional_3.py
+++ python/task_additional_3.py
@@ -1,3 +1,7 @@
+"""
+Task: Make a multiplication table from 1 to 10. Using a loop, write numbers 1 to 10 vertically and horizontally. At the intersection, enter the multiplication result.
+"""
+
 print ("----------------------Multiplication table--------------------")
 print(60*"-")
 for y in range(1,11):
