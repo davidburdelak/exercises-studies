d377c89fdb6ccfff459aa5b8b200216e5902fd26
David Burdelak <davidburdelak@gmail.com>
2020-01-17 18:48:22 +0100

    Code cleaning

--- python/task_5_1.py
+++ python/task_5_1.py
@@ -1,6 +1,5 @@
 """
 Task: Sergeant Thomson decided to censor the letters his soldiers receive. This censorship involves removing every third line from the letter. Help the sergeant: write a program that receives a text file with a letter and creates its censored version - task_5_1_letter_censor.txt.
-
 The task_5_1_letter.txt file exists before, and the task_5_1_letter_censor.txt file is created by the program.
 """
 
@@ -11,45 +10,45 @@ i=1
 all_lines = file_read.readlines()
 
 for line in all_lines: #Reading all lines from a text file
-    if (i==1):
+    if i==1:
         file_write.write(line)
         i+=1
-    elif (i==2):
+    elif i==2:
         file_write.write(line)
         i+=1
-    elif (i==3):
+    elif i==3:
         i+=1    
-    elif (i==4):
+    elif i==4:
         file_write.write(line)
         i+=1
-    elif (i==5):
+    elif i==5:
         file_write.write(line)
         i+=1
-    elif (i==6):
+    elif i==6:
         i+=1    
-    elif (i==7):
+    elif i==7:
         file_write.write(line)
         i+=1
-    elif (i==8):
+    elif i==8:
         file_write.write(line)
         i+=1
-    elif (i==9):
+    elif i==9:
         i+=1    
-    elif (i==10):
+    elif i==10:
         file_write.write(line)
         i+=1
-    elif (i==11):
+    elif i==11:
         file_write.write(line)
         i+=1
-    elif (i==12):
+    elif i==12:
         i+=1    
-    elif (i==13):
+    elif i==13:
         file_write.write(line)
         i+=1
-    elif (i==14):
+    elif i==14:
         file_write.write(line)
         i+=1     
-    elif (i==15):
+    elif i==15:
         i=1       
              
 file_write.close()
