cd08bef1aed7256ce4e014653ecdc4e3f71a8ea5
David Burdelak <davidburdelak@gmail.com>
2020-01-17 19:44:02 +0100

    Code cleaning

--- python/task_additional_2.py
+++ python/task_additional_2.py
@@ -1,3 +1,7 @@
+"""
+Draw a Christmas tree from triangles using the # (hash) sign in such a way that each level of the Christmas tree is 1 line longer than the previous one. At first, the script should ask about the height of the Christmas tree (number of triangles) and then draw it.
+"""
+
 level=int(input("Enter the level of the Christmas tree to generate: "))
 height=3
 for i in range(0, level):
